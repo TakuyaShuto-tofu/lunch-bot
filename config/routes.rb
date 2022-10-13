3ffde7f010d01d2d0ec65cf917e471961e3dd9c3
takuyashutoo <95073698+takuyashutoo@users.noreply.github.com>
2022-10-14 01:15:06 +0900

    homesコントローラーの作成、ログイン後の画面遷移先を変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   #controllers:{registrations:'registrations', sessions: :sessions}
   #root 'コントローラー名#アクション名'でサーバーを立てた最初の画面を設定
-  root 'stores#index'
   resources :stores
+  resources :homes
+  
+  root 'homes#index'
 end
